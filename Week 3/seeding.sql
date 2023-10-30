7a40fe70ad245e864bb9c1176e9cedea21b35e2f
nb3105 <nicolas.bens@ou.ac.uk>
2023-10-30 17:21:01 +0100

    all

---
+++ Week 3/seeding.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO cars (id, vin, 
+
+d	vin	manufacturer	model	year	color
+1	3K096I98581DHSNUP	Volkswagen	Tiguan	2019	Blue
+2	ZM8G7BEUQZ97IH46V	Peugeot	Rifter	2019	Red
+3	RKXVNNIHLVVZOUB4M	Ford	Fusion	2018	White
+4	HKNDGS7CU31E9Z7JW	Toyota	RAV4	2018	Silver
+5	DAM41UDN3CHU2WVF6	Volvo	V60	2019	Gray
+6	DAM41UDN3CHU2WVF6	Volvo	V60 Cross Country	2019	Gray
