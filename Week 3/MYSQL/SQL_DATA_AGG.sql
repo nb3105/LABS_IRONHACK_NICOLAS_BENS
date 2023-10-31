81c7e0750cd90bcf47226ba4de0ecff0aa588926
nb3105 <nicolas.bens@ou.ac.uk>
2023-10-31 16:32:09 +0100

    Update SQL_DATA_AGG.sql

--- Week 3/MYSQL/SQL_DATA_AGG.sql
+++ Week 3/MYSQL/SQL_DATA_AGG.sql
@@ -10,7 +10,20 @@ from film
 ORDER BY length ASC
 LIMIT 1;
 
-SELECT ROUND(AVG(length))
+SELECT ROUND(AVG(length)) as avg_d
 FROM film
 Order BY length
-LIMIT 1
+LIMIT 1;
+
+SELECT rental_date, return_date
+FROM rental
+ORDER BY DATEDIFF(rental_date, return_date) DESC;
+
+SELECT
+    rental_id, rental_date
+FROM rental
+LIMIT 20;
+
+SELECT ROUND(AVG(length),2) as avg_d, rating
+from film
+GROUP BY rating
