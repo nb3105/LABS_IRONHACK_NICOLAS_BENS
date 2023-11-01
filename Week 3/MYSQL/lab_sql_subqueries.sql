3dff32f484bba57de99f68c2d4e6b424bf6566c2
nb3105 <nicolas.bens@ou.ac.uk>
2023-11-01 15:08:08 +0100

    Create lab_sql_subqueries.sql

---
+++ Week 3/MYSQL/lab_sql_subqueries.sql
@@ -0,0 +1,35 @@
+#1 Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+
+SELECT COUNT(rental_id)
+FROM rental 
+WHERE inventory_id IN (SELECT inventory_id
+					FROM inventory 
+					WHERE film_id = (SELECT film_id
+										FROM film
+										WHERE title = "Hunchback Impossible"));
+                                        
+#2 List all films whose length is longer than the average length of all the films in the Sakila database.
+
+SELECT AVG(length)
+FROM film
+WHERE length IN (SELECT film_id
+					FROM film
+                    WHERE length);
+                    
+SELECT title, length
+FROM film
+WHERE length > (SELECT AVG(length) 
+					FROM film);
+                    
+                    
+#3 Use a subquery to display all actors who appear in the film "Alone Trip".
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN (SELECT actor_id
+				FROM film_actor
+				WHERE film_id = (SELECT film_id
+									FROM film
+									WHERE title = "Alone Trip"));
+                                    
+
