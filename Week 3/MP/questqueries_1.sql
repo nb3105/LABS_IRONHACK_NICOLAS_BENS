7e0dcbd190b3684d6914b52ee7ec0872ee6b0249
nb3105 <nicolas.bens@ou.ac.uk>
2023-11-02 16:30:09 +0100

    Update questqueries_1.sql

--- Week 3/MP/questqueries_1.sql
+++ Week 3/MP/questqueries_1.sql
@@ -28,3 +28,26 @@ FROM (
 ) AS age_group_data
 GROUP BY age_group;
 
+SELECT 
+    MAX(age) AS max_age,
+    MIN(age) AS min_age,
+    AVG(age) AS mean_age,
+    STD(age) AS std_age
+FROM medical;
+
+
+SELECT bmi_category, 
+       CASE 
+           WHEN age >= 25 AND age < 35 THEN '25-35'
+           WHEN age >= 35 AND age < 45 THEN '35-45'
+           ELSE '45+'
+       END AS age_group,
+       round(AVG(sleep_duration),2) AS average_sleep_duration
+FROM medical
+WHERE bmi_category = "Normal" or bmi_category = "Overweight" or bmi_category = "Obese"
+GROUP BY bmi_category, age_group;
+
+
+
+
+
