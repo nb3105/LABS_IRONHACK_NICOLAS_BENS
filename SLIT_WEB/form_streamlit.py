020f92d65247ead7c11eb29faa57939c63a64f93
nb3105 <nicolas.bens@ou.ac.uk>
2023-12-12 16:02:21 +0100

    a

--- SLIT_WEB/form_streamlit.py
+++ SLIT_WEB/form_streamlit.py
@@ -8,15 +8,29 @@ if 'stage' not in st.session_state:
 def set_stage(stage):
     st.session_state.stage = stage
 
+# Define the fixed questions
+questions = [
+    "How satisfied are you with the cleanliness of the airport?",
+    "How satisfied are you with the customer service?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?",
+    "How satisfied are you with the facilities?"
+    # Add more questions as needed
+]
+
 with st.form(key='my_form'):
-    n_forms = 10  # Fixed number of questions
-    answers = [st.slider(f'Question {i+1}', 0, 5, 2) for i in range(n_forms)]
+    answers = [st.slider(question, 0, 5, 2) for question in questions]
 
     submit_button = st.form_submit_button(label='Submit forms', on_click=set_stage, args=(1,))
 
 if st.session_state.stage > 0:
     st.write("User responses:")
-    for i, answer in enumerate(answers):
-        st.write(f'Question {i+1}: {answer}')
+    for i, (question, answer) in enumerate(zip(questions, answers)):
+        st.write(f'{question}: {answer}')
 
     st.button('Reset', on_click=set_stage, args=(0,))
